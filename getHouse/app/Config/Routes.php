55a1cf36089900e1eedb3d9caffbd60332f10306
b4yuPraze <bayuprasetyo.bp11@gmail.com>
2024-03-20 08:12:18 +0700

    Update Routes

--- getHouse/app/Config/Routes.php
+++ getHouse/app/Config/Routes.php
@@ -6,3 +6,21 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+
+/* Routes Administrator
+
+    [ Controller View Admin ]
+    
+*/
+$routes->get("administrator", "Admin::index");
+$routes->get("administrator/kelolaSertifikat", "Admin::manageCertificate");
+$routes->get("administrator/kelolaPeserta", "Admin::manageParticipants");
+$routes->get("administrator/kelolaPengguna", "Admin::manageUsers");
+
+/* Routes Administrator
+
+    [ Controller Functions Admin ]
+    
+*/
+$routes->post('administrator/addUsers', 'Admin::addUsers');
